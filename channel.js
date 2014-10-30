44649661886bed0360a78be34ba3a28e708e6a31
James Cready <jcready@gmail.com>
2014-10-30 12:58:24 -0400

    Clean up optional logging and add more tests

--- channel.js
+++ channel.js
@@ -5,27 +5,28 @@ module.exports = function createChannel(url, assertions, log){
   assertions = assertions || {};
   log = log || { info: noop, warn: noop, error: noop };
 
-  return require('amqplib').connect(url)
-    .then(openChannel, log.error.bind(log, 'RabbitMQ'));
+  return require('amqplib').connect(url).then(openChannel, log.error);
 
   function openChannel(connection) {
+    var amqp = require('url').parse(url);
+    var user = amqp.auth.split(':')[0];
     var close = connection.close.bind(connection);
-    log.info('RabbitMQ', 'Connected to %s', url);
+    log.info('Connected to %s as "%s"', amqp.host, user);
     process.once('SIGINT', close);
     process.once('SIGTERM', close);
-    return connection.createConfirmChannel().then(assertChannel, close);
+    return connection.createConfirmChannel().then(assertChannelMethods, close);
   }
 
-  function assertChannel(channel) {
+  function assertChannelMethods(channel) {
     var setup = [];
     for (var fn in assertions) {
       setup.push.apply(setup, assertions[fn].map(function invocation(args){
-        log.info('RabbitMQ', 'Channel %s(%j)', fn, args);
+        log.info('- Channel %s(%j)', fn, args);
         return channel[fn].apply(channel, args);
       }));
     };
 
-    channel.on('error', log.error.bind(log, 'RabbitMQ'));
+    channel.on('error', log.error);
     channel.on('blocked', blocked(true));
     channel.on('unblocked', blocked(false));
     channel.isBlocked = false;
@@ -35,12 +36,12 @@ module.exports = function createChannel(url, assertions, log){
       .catch(closeChannel);
 
     function returnChannel(){
-      log.info('RabbitMQ', 'Channel assertions succeeded');
+      log.info('- Channel setup complete');
       return channel;
     }
 
     function closeChannel(error){
-      log.error('RabbitMQ', 'Channel assertions failed', error);
+      log.error('- Channel assertions failed', error);
       channel.close();
       throw error;
     }
@@ -49,7 +50,7 @@ module.exports = function createChannel(url, assertions, log){
       return function changeState(){
         var state = isBlocked ? 'blocked' : 'unblocked';
         var level = isBlocked ? 'warn' : 'info';
-        log[level]('RabbitMQ', 'Channel %s', state);
+        log[level]('- Channel %s', state);
         channel.isBlocked = isBlocked;
       };
     }
--- test/test.channel.js
+++ test/test.channel.js
@@ -1,5 +1,5 @@
 /*jslint nodejs: true, expr: true*/
-/*global describe: true, it: true, before: true*/
+/*global describe: true, it: true, before: true, after: true*/
 
 process.env.NODE_ENV = 'test';
 
@@ -54,14 +54,52 @@ var assertions = {
   deleteQueue    : [['queue', { ifEmpty: true }]]
 };
 
-_.merge(channel, _.mapValues(assertions, function(){
-  return sinon.stub().returns(Promise.resolve());
-}));
-
 describe('Channel', function() {
   var getChannel = null;
-  describe('upon successful setup', function(){
+  describe('with no assertions', function(){
     before(function(){
+      getChannel = proxyquire('../channel', {
+        amqplib: amqplib
+      })(amqpUrl);
+      return getChannel;
+    });
+
+    it('should connect to the correct RabbitMQ URL', function(){
+      expect(connect).to.have.been.calledOnce;
+      expect(connect).to.have.been.calledWithExactly(amqpUrl);
+    });
+
+    it('should create a Channel on the Connection', function(){
+      expect(createConfirmChannel).to.have.been.calledOnce;
+      expect(createConfirmChannel).to.have.been.calledWithExactly();
+    });
+
+    it('should mark the Channel as blocked when it gets a blocked event', function(){
+      channel.emit('blocked');
+      expect(channel).to.have.property('isBlocked').that.is.true;
+    });
+
+    it('should mark the Channel as unblocked when it gets a blocked event', function(){
+      channel.emit('unblocked');
+      expect(channel).to.have.property('isBlocked').that.is.false;
+    });
+
+    it('should resolve with a Rabbit MQ Channel', function(){
+      return expect(getChannel).to.eventually.eql(channel);
+    });
+
+    after(function(){
+      connect.reset();
+      createConfirmChannel.reset();
+    });
+  });
+
+  describe('with valid assertions', function(){
+    before(function(){
+      _.merge(channel, _.mapValues(assertions, function(){
+        return sinon.stub().returns(Promise.resolve());
+      }));
+
       getChannel = proxyquire('../channel', {
         amqplib: amqplib
       })(amqpUrl, assertions, npmlog);
@@ -108,7 +146,7 @@ describe('Channel', function() {
     });
   });
 
-  describe('upon failed setup', function(){
+  describe('with invalid assertions', function(){
     before(function (done){
       var test = done.bind(null, null);
       channel.assertExchange = channel.assertQueue = 
@@ -119,12 +157,18 @@ describe('Channel', function() {
       getChannel.then(test).catch(test);
     });
 
+    it('should log an error', function(){
+      expect(npmlog.error).to.have.been.calledOnce;
+      expect(npmlog.error.firstCall.args[1]).to.be.an.instanceOf(Error);
+    })
+
     it('should close the channel', function(){
       expect(channel.close).to.have.been.calledOnce;
+      expect(channel.close).to.have.been.calledWithExactly();
     });
 
     it('should reject with an error', function(){
-      expect(getChannel).to.be.rejected;
+      expect(getChannel).to.be.rejectedWith(Error);
     });
   });
 });

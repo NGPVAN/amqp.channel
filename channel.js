afff2f91b5063ddb4c9925875f7c2dc55aa01238
Josh Fitch <jfitch@ngpvan.com>
2023-04-13 11:21:44 -0400

    VAN-48271: Adjustments

--- channel.js
+++ channel.js
@@ -1,11 +1,11 @@
-var noop = function(){},
-    util = require('util'),
-    amqp = require('amqplib'),
-    Promise = require('bluebird'),
-    simplify = require('./simplify'),
-    uriJs = require('uri-js');
+var noop = function () { },
+  util = require('util'),
+  amqp = require('amqplib'),
+  Promise = require('bluebird'),
+  simplify = require('./simplify'),
+  uriJs = require('uri-js');
 
-module.exports = function createChannel(url, assertions, log, socketOptions, defaultServernameToHostname){
+module.exports = function createChannel(url, assertions, log, socketOptions, defaultServernameToHostname) {
   assertions = assertions || {};
   log = log || { info: noop, warn: noop, error: noop };
   socketOptions = socketOptions || {};
@@ -20,20 +20,20 @@ module.exports = function createChannel(url, assertions, log, socketOptions, def
   }
 
   function retryConnection(err) {
-    log.info('AMQP channel error or disconnection, retrying', err);
-    return new Promise(setTimeout(() => amqp.connect(url, socketOptions).then(openChannel), 60000));
-}
+    console.info('AMQP channel error or disconnection, retrying', err);
+    return new Promise(setTimeout(() => amqp.connect(url, socketOptions).then(openChannel), 1000));
+  }
 
   return amqp.connect(url, socketOptions).then(openChannel);
 
   function openChannel(connection) {
     var amqp = require('url').parse(url);
     var user = amqp.auth.split(':')[0];
-    var close = function closeConnection(e){
+    var close = function closeConnection(e) {
       var close = Promise.resolve(connection.close());
       return e ? close.throw(e) : /* istanbul ignore next */ close;
     };
-    log.info('Connected to %s as "%s"', amqp.host, user);
+    console.info('Connected to %s as "%s"', amqp.host, user);
     process.once('SIGINT', close);
     process.once('SIGTERM', close);
     connection.once("close", retryConnection);
@@ -58,35 +58,41 @@ module.exports = function createChannel(url, assertions, log, socketOptions, def
     channel.on('unblocked', blocked(false));
     if (!channel.hasOwnProperty('isBlocked')) {
       Object.defineProperty(channel, 'isBlocked', {
-        get: function(){ return channelIsBlocked },
+        get: function () { return channelIsBlocked },
         enumerable: true
       });
     }
 
-    return Promise.all(setup).then(returnChannel, closeChannel);
+    return Promise.all(setup).then(returnChannelFactory, closeChannel);
 
-    function applyToChannel(method){
-      return function invocation(args){
-        log.info('- Channel %s(%j)', method, args);
+    function applyToChannel(method) {
+      return function invocation(args) {
+        console.info('- Channel %s(%j)', method, args);
         return channel[method].apply(channel, args);
       }
     }
 
-    function returnChannel(){
-      log.info('- Channel setup complete');
+    function returnChannel() {
+      console.info('- Channel setup complete');
       return simplify(channel);
     }
 
-    function closeChannel(error){
+    function returnChannelFactory() {
+      return function () {
+        return simplify(channel);
+      }
+    }
+
+    function closeChannel(error) {
       log.error('- Channel assertions failed', error);
       channel.close();
       return Promise.reject(error);
     }
 
-    function blocked(isBlocked){
+    function blocked(isBlocked) {
       var state = isBlocked ? 'blocked' : 'unblocked';
       var level = isBlocked ? 'warn' : 'info';
-      return function changeBlockedState(){
+      return function changeBlockedState() {
         log[level]('- Channel %s', state);
         channelIsBlocked = isBlocked;
       };
